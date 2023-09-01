a3e6fc3fc9f9339f096c2a20c724cf161ed511ce
PHILO-HE <feilong.he@intel.com>
2023-09-01 15:14:10 +0800

    Change velox branch for test

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=update
+VELOX_REPO=https://github.com/PHILO-HE/velox.git
+VELOX_BRANCH=enable-null-type
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
