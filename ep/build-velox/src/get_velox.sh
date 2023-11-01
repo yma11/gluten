fe4e1d4616cd60dbab4414283d210c22278c9e26
yan ma <yan.ma@intel.com>
2023-11-02 05:40:36 +0800

    switch to corresponding velox

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/PHILO-HE/velox.git
-VELOX_BRANCH=enable-null-type
+VELOX_REPO=https://github.com/yma11/velox.git
+VELOX_BRANCH=golden-ptst
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
