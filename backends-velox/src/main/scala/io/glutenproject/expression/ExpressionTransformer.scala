2408d17ff308a2d4b60477e76eb50ca69d29b815
yan ma <yan.ma@intel.com>
2023-11-02 04:48:56 +0800

    split change

--- backends-velox/src/main/scala/io/glutenproject/expression/ExpressionTransformer.scala
+++ backends-velox/src/main/scala/io/glutenproject/expression/ExpressionTransformer.scala
@@ -133,7 +133,7 @@ case class VeloxStringSplitTransformer(
 
     val limit = limitExpr.doTransform(args).asInstanceOf[IntLiteralNode].getValue
     val regex = regexExpr.doTransform(args).asInstanceOf[StringLiteralNode].getValue
-    if (limit > 0 || regex.length > 1) {
+    if (limit > 0) {
       throw new UnsupportedOperationException(
         s"$original supported single-length regex and negative limit, but given $limit and $regex")
     }
