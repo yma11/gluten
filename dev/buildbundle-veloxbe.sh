dea03585493515e13cad7bd40f0eabfc5f57ce37
yan ma <yan.ma@intel.com>
2023-11-09 00:42:29 +0800

    fix for pinterest spark

--- dev/buildbundle-veloxbe.sh
+++ dev/buildbundle-veloxbe.sh
@@ -4,6 +4,4 @@ BASEDIR=$(dirname $0)
 source "$BASEDIR/builddeps-veloxbe.sh"
 
 cd $GLUTEN_DIR
-mvn clean package -Pbackends-velox -Prss -Pspark-3.2 -DskipTests
-mvn clean package -Pbackends-velox -Prss -Pspark-3.3 -DskipTests
-mvn clean package -Pbackends-velox -Prss -Pspark-3.4 -DskipTests
+mvn clean package -Pbackends-velox -Prss -Pspark-3.2 -DskipTests  -Dcheckstyle.skip=true -DskipScalastyle=true
