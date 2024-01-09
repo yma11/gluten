73db0e955d65629aa30cad95c7fad0282439c02e
yan ma <yan.ma@intel.com>
2024-01-10 04:56:18 +0800

    disable memory leak check

--- cpp/velox/memory/VeloxMemoryManager.cc
+++ cpp/velox/memory/VeloxMemoryManager.cc
@@ -145,7 +145,7 @@ VeloxMemoryManager::VeloxMemoryManager(
   velox::memory::MemoryManagerOptions mmOptions{
       .alignment = velox::memory::MemoryAllocator::kMaxAlignment,
       .trackDefaultUsage = true, // memory usage tracking
-      .checkUsageLeak = true, // leak check
+      .checkUsageLeak = false, // leak check
       .debugEnabled = false, // debug
       .coreOnAllocationFailureEnabled = false,
       .allocatorCapacity = velox::memory::kMaxMemory,
