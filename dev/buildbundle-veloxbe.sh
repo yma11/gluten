eb5254aa9d40bdbe0333f7644cbf823a50c7628d
binwei <binwei.yang@intel.com>
2024-02-20 19:55:21 +0000

    add byte type support  fix the flatten bug

--- dev/buildbundle-veloxbe.sh
+++ dev/buildbundle-veloxbe.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname $0)
 source "$BASEDIR/builddeps-veloxbe.sh"
 
 cd $GLUTEN_DIR
-mvn clean package -Pbackends-velox  -Pceleborn -Puniffle -Pspark-3.2 -DskipTests  -Dcheckstyle.skip=true -DskipScalastyle=true
+mvn clean package -Pbackends-velox  -Pceleborn -Puniffle -Pspark-3.2 -Piceberg -DskipTests  -Dcheckstyle.skip=true -DskipScalastyle=true
