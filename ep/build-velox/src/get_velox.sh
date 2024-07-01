4bbe51cce7af62cfb85584aea24ea036f5023caa
binwei <binwei.yang@intel.com>
2024-07-01 23:27:22 +0000

    fix for pinterest build

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_06_30
+VELOX_REPO=https://github.com/yma11/velox.git
+VELOX_BRANCH=golden-ptst
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
